cd7304de691bc1fa377f51d90eec8e38b8308267
will-oracions <Oracions.dev@gmail.com>
2023-02-22 20:18:37 +0100

    save

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
   return <>
     <Box bg="gray.200" py="1rem" zIndex="10">
       <AppContainer>
-        <Flex w="100%" h="100%" alignItems="center" justifyContent="center">
+        <Flex w="100%" h="100%" fontSize={{base: '12px', md: '14px' }} alignItems="center" justifyContent="center">
           <Image w="30px" h="30px" src="/assets/images/art.png" alt="art" />
           <Text>Copy Right  &copy; -</Text>
           <Text><Link href="https://will-oracions.pages.dev" target="blank">&nbsp;will-oracions.pages.dev</Link> 2023</Text>
